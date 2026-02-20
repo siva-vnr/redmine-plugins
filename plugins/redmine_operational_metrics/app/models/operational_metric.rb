50ba65a976f7b5088c1fa1ef3cffa1224d6a6426
siva-vnr <siva@superbotics.in>
2026-02-20 12:16:48 +0530

    feat: Allow `spent_time` input in `HH.MM` format and display it consistently across views.

--- plugins/redmine_operational_metrics/app/models/operational_metric.rb
+++ plugins/redmine_operational_metrics/app/models/operational_metric.rb
@@ -2,6 +2,22 @@ class OperationalMetric < ActiveRecord::Base
   validates_presence_of :task_id, :user_name, :completion
   validates_inclusion_of :completion, in: ['Break down', 'Break through'], message: "%{value} is not a valid completion status"
   
-  # Allow team assignment
-  # attr_accessible :team # Not needed in Rails 5+ with strong params, but good for clarity if using protected_attributes gem
+  # Override spent_time= to handle HH.MM input
+  def spent_time=(value)
+    if value.is_a?(String) && value.include?('.')
+      hours, minutes = value.split('.').map(&:to_i)
+      total_minutes = (hours * 60) + (minutes || 0)
+      write_attribute(:spent_time, total_minutes)
+    else
+      write_attribute(:spent_time, value)
+    end
+  end
+
+  # Helper to format integer minutes back to HH:MM or HH.MM
+  def formatted_spent_time
+    return "0.00" if spent_time.blank?
+    hours = spent_time / 60
+    minutes = spent_time % 60
+    "#{hours}.#{minutes.to_s.rjust(2, '0')}"
+  end
 end
