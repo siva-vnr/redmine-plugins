60a546580f4dd911e7a6a404c14ad5f85521cea5
siva-vnr <siva@superbotics.in>
2026-02-21 13:08:55 +0530

    refactor: Change `spent_time` unit from minutes to decimal hours across schema, model, and views.

--- plugins/redmine_operational_metrics/app/models/operational_metric.rb
+++ plugins/redmine_operational_metrics/app/models/operational_metric.rb
@@ -2,28 +2,17 @@ class OperationalMetric < ActiveRecord::Base
   validates_presence_of :task_id, :user_name, :completion
   validates_inclusion_of :completion, in: ['Break down', 'Break through'], message: "%{value} is not a valid completion status"
   
-  # Override spent_time= to handle HH.MM input or plain HH
+  # Override spent_time= to handle decimal hours or H:MM input
   def spent_time=(value)
     if value.is_a?(String)
-      if value.include?('.')
-        hours, minutes = value.split('.').map(&:to_i)
-        total_minutes = (hours * 60) + (minutes || 0)
-        write_attribute(:spent_time, total_minutes)
+      if value.include?(':')
+        hours, minutes = value.split(':').map(&:to_f)
+        write_attribute(:spent_time, hours + (minutes / 60.0))
       else
-        write_attribute(:spent_time, value.to_i * 60)
+        write_attribute(:spent_time, value.to_f)
       end
-    elsif value.is_a?(Numeric)
-      write_attribute(:spent_time, (value * 60).to_i)
     else
       write_attribute(:spent_time, value)
     end
-  end
-
-  # Helper to format integer minutes back to HH:MM or HH.MM
-  def formatted_spent_time
-    return "0.00" if spent_time.blank?
-    hours = spent_time / 60
-    minutes = spent_time % 60
-    "#{hours}.#{minutes.to_s.rjust(2, '0')}"
   end
 end
--- plugins/redmine_operational_metrics/db/migrate/001_create_operational_metrics.rb
+++ plugins/redmine_operational_metrics/db/migrate/001_create_operational_metrics.rb
@@ -6,7 +6,7 @@ class CreateOperationalMetrics < ActiveRecord::Migration[5.2]
       t.string :project
       t.text :task_planed
       t.date :task_date
-      t.integer :spent_time
+      t.decimal :spent_time
       t.text :spent_detail
       t.string :collaborate_with
       t.string :completion, null: false
